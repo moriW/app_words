ae1bd2da592dd264b2cb67c5f5d6f043bb712699
Mori <yuhua.zhao@dragonplus.com>
2021-11-18 18:01:57 +0800

    update code

--- entry.py
+++ entry.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 from utils import logit
 from scraper import scraper
 from nlp import build_tfidf
@@ -12,8 +13,8 @@ google_package, apple_name = apps[0], apps[1]
 
 @logit
 def scrap():
-    with open("documents", "w") as _f:
-        for review_content in scraper(google_package, apple_name):
+    with open("documents2", "w") as _f:
+        for review_content in scraper(google_package, apple_name, count=1000):
             _f.write(review_content.strip() + "\n")
 
 
@@ -28,7 +29,8 @@ def nlp():
 if __name__ == "__main__":
     scrap()
     # nlp()
-    # keybert_model = KeyBERT()
-    # with open("documents", "r") as _rf:
-    #     content = _rf.readlines()
-    #     keybert_model.extract_keywords(content, keyphrase_ngram_range=(1, 2))
+    keybert_model = KeyBERT()
+    with open("documents2", "r") as _rf, open("keyword.p", "wb") as _wf:
+        content = _rf.read()
+        keywords = keybert_model.extract_keywords(content, keyphrase_ngram_range=(1, 2), top_n=1000)
+        pickle.dump(keywords, _wf)
