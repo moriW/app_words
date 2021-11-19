bde459c2510a18b140fba1069b43e331fd8a68f9
Mori <yuhua.zhao@dragonplus.com>
2021-11-19 17:34:53 +0800

    update file

--- entry.py
+++ entry.py
@@ -1,9 +1,13 @@
+import os
 import sys
 import pickle
+
 from utils import logit
 from scraper import scraper
 from nlp import build_tfidf
 
+
+import nltk
 from keybert import KeyBERT
 
 args = sys.argv[1:]
@@ -11,26 +15,55 @@ apps = (args + [None, None])[:2]
 google_package, apple_name = apps[0], apps[1]
 
 
+DOCUMENTS_FOLDER = "documents"
+KEYWORDS_FOLDER = "keywords"
+
+
+for folder in [DOCUMENTS_FOLDER, KEYWORDS_FOLDER]:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+document_path = os.path.join(DOCUMENTS_FOLDER, apple_name)
+keyword_path = os.path.join(KEYWORDS_FOLDER, apple_name)
+
+
 @logit
 def scrap():
-    with open("documents2", "w") as _f:
+    if os.path.exists(document_path):
+        return
+    with open(document_path, "w") as _f:
         for review_content in scraper(google_package, apple_name, count=1000):
-            _f.write(review_content.strip() + "\n")
+            _f.write(review_content.lower().strip() + "\n")
 
 
 @logit
-def nlp():
-    with open("documents", "r") as _rf, open("tfidf", "w") as _wf:
-        content = _rf.readlines()
-        for word in build_tfidf(content):
-            _wf.write(word + "\n")
+def nlp1():
+    ps = nltk.PorterStemmer()
+    keybert_model = KeyBERT()
+    value_dict = {}
+    with open(document_path, "r") as _f:
+        content = _f.readlines()
+        for raw_content, keyword_value_pair in zip(
+            content, keybert_model.extract_keywords(content, top_n=100)
+        ):
+            keyword = [x[0] for x in keyword_value_pair]
+            stem_tag = {
+                ps.stem(word): tag
+                for word, tag in nltk.pos_tag(nltk.tokenize.word_tokenize(raw_content))
+                if word in keyword
+            }
+
+            for keyword, value in keyword_value_pair:
+                stem = ps.stem(keyword)
+                key = f"{stem} {stem_tag.get(stem, None)}"
+                value_dict[key] = value_dict.get(key, {"count": 0, "value": 0})
+                value_dict[key]["count"] += 1
+                value_dict[key]["value"] += value
+
+    with open(keyword_path, "wb") as _f:
+        pickle.dump(value_dict, _f)
 
 
 if __name__ == "__main__":
     scrap()
-    # nlp()
-    keybert_model = KeyBERT()
-    with open("documents2", "r") as _rf, open("keyword.p", "wb") as _wf:
-        content = _rf.read()
-        keywords = keybert_model.extract_keywords(content, keyphrase_ngram_range=(1, 2), top_n=1000)
-        pickle.dump(keywords, _wf)
+    nlp1()
--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import itertools
+from typing import Optional
 
 import demoji
 
@@ -7,6 +8,14 @@ from .apple import scraper_apple
 from .google import scraper_google
 
 __all__ = ["scraper", "scraper_google", "scraper_apple"]
+
+
+def content_filter(content: str) -> Optional[str]:
+    content = demoji.replace(content)
+    if len(content) < 20:
+        return None
+    content = " ".join(filter(lambda x: len(x) < 15, content.split()))
+    return content
 
 
 def scraper(
@@ -19,18 +28,11 @@ def scraper(
     for lan, country in itertools.product(lans, countries):
         logging.info(f"read reviews on {lan}, {country} @ google")
         for review in scraper_google(google_package, lan, country, count):
-            review = demoji.replace(review)
-            if len(review) < 20:
-                continue
-            review = " ".join(filter(lambda x: len(x) < 15, review.split()))
-
-            yield review
+            review = content_filter(review)
+            if review: yield review
 
     for country in countries:
         logging.info(f"read reviews on {country} @ apple")
         for review in scraper_apple(apple_name, country, count):
-            review = demoji.replace(review)
-            review = " ".join(filter(lambda x: len(x) < 15, review.split()))
-            if len(review) < 20:
-                continue
-            yield review
+            review = content_filter(review)
+            if review: yield review
